7c549d058c576ef4a93ad84da8183c6206f3772a
BrentSoler <brntsoler@gmail.com>
2022-07-04 17:13:32 +0800

    for deployment

--- components/Nav/index.jsx
+++ components/Nav/index.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import Icon from "../../public/Icon.webp";
 import { AiOutlineMenuUnfold } from "react-icons/ai";
 import { useSelector, useDispatch } from "react-redux";
@@ -20,18 +21,18 @@ const NavBar = ({ children }) => {
 			<input id="sidemenu" type="checkbox" className="drawer-toggle" />
 			<div className="drawer-content w-full">
 				<div className="flex justify-between items-center relative">
-					<a href="/">
+					<Link href="/">
 						<div className="flex items-center mt-4 ml-4 space-x-2">
 							<Image src={Icon} width={65} height={65} />
 							<h1 className="text-4xl font-bold">Notes.</h1>
 						</div>
-					</a>
+					</Link>
 					<label className="drawer-button absolute right-4 top-8 sm:hidden" htmlFor="sidemenu">
 						<AiOutlineMenuUnfold className="text-4xl" />
 					</label>
 					<ul className="mt-4 mr-4 space-x-3 hidden sm:block">
-						{!user && <a href="/login">Login</a>}
-						{!user && <a href="/register">Register</a>}
+						{!user && <Link href="/login">Login</Link>}
+						{!user && <Link href="/register">Register</Link>}
 						{user && <button onClick={logoutHandler}>Logout</button>}
 					</ul>
 				</div>
@@ -41,8 +42,8 @@ const NavBar = ({ children }) => {
 			<div className="drawer-side">
 				<label htmlFor="sidemenu" className="drawer-overlay"></label>
 				<ul className="menu bg-base-100 overflow-y-auto w-[50%] p-5 font-semibold text-xl space-y-5 flex flex-col justify-center z-[999]">
-					{!user && <a href="/login">Login</a>}
-					{!user && <a href="/register">Register</a>}
+					{!user && <Link href="/login">Login</Link>}
+					{!user && <Link href="/register">Register</Link>}
 					{user && <label onClick={logoutHandler}>Logout</label>}
 				</ul>
 			</div>
